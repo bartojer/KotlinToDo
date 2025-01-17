89989a7b72f57ca0c0a2c7da650c9a252c465420
bartojer <bartojer@gmail.com>
2025-01-17 12:37:45 -0700

    I'm not a big fan of soda

---
+++ menu.kt
@@ -0,0 +1,17 @@
+class menu {
+    fun edit() {
+        println("Edit item(s)")
+    }
+    fun display() {
+        println("View list here")
+    }
+    fun add() {
+        println("Add new item(s)")
+    }
+    fun delete() {
+        println("Remove item(s)")
+    }
+    fun complete() {
+        println("Marked as complete")
+    }
+}
